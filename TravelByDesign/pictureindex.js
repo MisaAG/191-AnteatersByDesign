e1cc24db7d4d741aa4e7b30612fd6de1fd5ead42
Harry Tjahja <harry.d.tjahja@gmail.com>
2020-05-20 21:41:19 -0700

    picture folder for app access

---
+++ TravelByDesign/pictureindex.js
@@ -0,0 +1,13 @@
+const images = {
+    pic : require("./asset/pic.jpg"),
+    bali : require("./asset/bali.jpg"),
+    kyoto : require("./asset/kyoto.jpg"),
+    goldengatepark : require("./asset/goldengatepark.jpg"),
+    park : require("./asset/park.jpeg"),
+    academyOfScience : require("./asset/academyOfScience.png"),
+    academysciencediver : require("./asset/academysciencediver.jpg"),
+    downtown : require("./asset/downtown.jpg"),
+    chinatown : require("./asset/chinatown.jpg"),
+    salesforce : require("./asset/salesforce.jpg")
+}
+export {images};
